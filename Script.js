64fef70eda29efc87bd7edf44cca38dd62304f81
Minirion <deschampsm.pro@gmail.com>
2020-10-08 15:16:17 +0200

    popup mail

--- Script.js
+++ Script.js
@@ -1,4 +1,12 @@
+// Menu burger 
 function onClickMenu() {
     document.getElementById("menu").classList.toggle("change");
     document.getElementById("navMenu").classList.toggle("change");
   }
+
+// Pop-Up mail 
+const messageForm = document.getElementById("messageForm");
+const name = document.getElementById(`name`);
+messageForm.addEventListener("click",function(){
+  alert(`Merci ${name.value}, que la force de Géo Pinchat soit avec toi, et la beauté de Tomme te comble (mais il est à moi) (je suis Marion) (Tomme si tu passes par là... <3)`);
+});
