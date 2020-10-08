984cb555d1048999765eb5445ebac34d1e7435c3
Léa Miquel <leamiquel21@gmail.com>
2020-10-08 15:15:55 +0200

    ajout bouton like aux articles

--- Script.js
+++ Script.js
@@ -1,4 +1,45 @@
+//menu burger
+
 function onClickMenu() {
     document.getElementById("menu").classList.toggle("change");
     document.getElementById("navMenu").classList.toggle("change");
+  }
+
+
+//like button 
+
+  var likeButton = document.getElementById("likeButton");
+  function onClickLike() {
+    if (likeButton.classList.contains("far")){
+      likeButton.classList.remove("far");
+      likeButton.classList.add("fas");
+    }
+    else{
+      likeButton.classList.remove("fas");
+      likeButton.classList.add("far");
+    }
+  }
+
+  var likeButton2 = document.getElementById("liikeButton");
+  function onClickLike2() {
+    if (likeButton2.classList.contains("far")){
+      likeButton2.classList.remove("far");
+      likeButton2.classList.add("fas");
+    }
+    else{
+      likeButton2.classList.remove("fas");
+      likeButton2.classList.add("far");
+    }
+  }
+
+  var likeButton3 = document.getElementById("liiikeButton");
+  function onClickLike3() {
+    if (likeButton3.classList.contains("far")){
+      likeButton3.classList.remove("far");
+      likeButton3.classList.add("fas");
+    }
+    else{
+      likeButton3.classList.remove("fas");
+      likeButton3.classList.add("far");
+    }
   }
